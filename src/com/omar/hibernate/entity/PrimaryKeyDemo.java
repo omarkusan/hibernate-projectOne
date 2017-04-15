4e16fc40a41091311c71897d56ecf411853ae1ea
Omar Mehmet <omarkusan@gmail.com>
2017-04-15 12:16:37 -0500

    Set PrimaryKey on source code

--- src/com/omar/hibernate/entity/PrimaryKeyDemo.java
+++ src/com/omar/hibernate/entity/PrimaryKeyDemo.java
@@ -1,10 +1,40 @@
 package com.omar.hibernate.entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class PrimaryKeyDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//create session factory
+				SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Student.class)
+										.buildSessionFactory();
+				//create session
+				Session session = factory.getCurrentSession();
+			try {
+				//create a student object
+				System.out.println("Creating 3 students objects.....");
+				Student tempStudent1 = new Student("Alim","Wall","alim@gmail.com");
+				Student tempStudent2 = new Student("Tarim","Wall","tarim@gmail.com");
+				Student tempStudent3 = new Student("Tursun","Wall","tursun@gmail.com");
 
+				//start a transaction
+				session.beginTransaction();
+				
+				//save the object
+				System.out.println("Saving the student...");
+				session.save(tempStudent1);
+				session.save(tempStudent2);
+				session.save(tempStudent3);
+				
+				//commit transaction
+				session.getTransaction().commit(); 
+			}finally {
+				factory.close();
+			}
 	}
 
 }
